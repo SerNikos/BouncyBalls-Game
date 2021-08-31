e9b301eee48f360a4effc8eb57deb9c0a9610a56
SerNikos <78097143+SerNikos@users.noreply.github.com>
2021-09-01 01:01:06 +0300

    This is the first completed version of the game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // when we tap the ball it gets destroyed
+    private void OnMouseDown()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().ScoreUp();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    int score = 0;
+    public GameObject WinText;
+    public GameObject RestartButton;
+
+
+    public void ScoreUp()
+    {
+        score++;
+        if (score == 4)
+        {
+            Win();
+        }
+    }
+    
+    void Win()
+    {
+        WinText.SetActive(true);
+        RestartButton.SetActive(true);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
